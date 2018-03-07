c653a52f5154596980ba89b70a3dd130e68e3466
Md. Jahid Hasan <mjhprince1@gmail.com>
2018-03-07 11:16:12 +0600

    Add files via upload

---
+++ BinSrc_pair_dfrnc.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+    int n,num,i,a[10000];
+    int mid=0,right,left,flag=0;
+    scanf("%d%d",&n,&num);
+    for(i=1; i<=n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int p=0,m=0;
+    for(i=1; i<n; i++)
+    {
+        right=n;
+        left=i+1;
+        while(left<=right)
+        {
+            mid=(left+right)/2;
+            if(a[i]+num==a[mid])
+            {
+                m=a[i];
+                p=a[mid];
+                flag=1;
+                break;//stop looping
+            }
+            else if(a[mid]>a[i]+num)//jogfol theke mid value boro hole
+            {
+                right=mid-1;
+            }
+
+            else
+            {
+                left=mid+1;
+            }
+
+        }
+        if(flag==1)
+            break;//stop looping
+
+    }
+    printf("%d %d",m,p);
+
+}
